d9ecc764b642f6e5218a23ae3ae8afaeff9e4b91
enigmoe <moritz.zajonz@protonmail.com>
2020-09-10 15:02:05 +0200

    fix code for multiple countries

--- 03_data_subsets.R
+++ 03_data_subsets.R
@@ -1,14 +1,14 @@
 # Data subsets for Google
 
 google <- google %>%
-  group_by(job, datetime) %>%
+  group_by(country, job, datetime) %>%
   mutate(clicks_rel = if_else(clicks > 0, clicks/sum(clicks), clicks),
          impr_rel = impressions/sum(impressions)) %>%
   ungroup()
 
 google_male_female <- google %>%
   filter(gender != "undetermined") %>%
-  group_by(job, datetime) %>%
+  group_by(country, job, datetime) %>%
   mutate(clicks_rel = if_else(clicks > 0, clicks/sum(clicks), clicks),
          impr_rel = impressions/sum(impressions)) %>%
   ungroup()
@@ -16,14 +16,14 @@ google_male_female <- google %>%
 # Data subsets for Facebook
 
 facebook <- facebook %>%
-  group_by(job, datetime) %>%
+  group_by(country, job, datetime) %>%
   mutate(clicks_rel = if_else(clicks > 0, clicks/sum(clicks), as.double(clicks)),
          impr_rel = impressions/sum(impressions)) %>%
   ungroup()
 
 facebook_male_female <- facebook %>%
   filter(gender != "undetermined") %>%
-  group_by(job, datetime) %>%
+  group_by(country, job, datetime) %>%
   mutate(clicks_rel = if_else(clicks > 0, clicks/sum(clicks), as.double(clicks)),
          impr_rel = impressions/sum(impressions)) %>%
   ungroup()
